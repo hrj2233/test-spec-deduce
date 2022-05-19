babd713ab73b8ce70007adb880b32f0b213ba950
hrj2233 <hrj2233@gmail.com>
2022-05-20 01:25:20 +0900

    deduce test specs.js

--- test/specs.js
+++ test/specs.js
@@ -67,9 +67,9 @@ describe("myRange(min, max, step)", () => {
       const [min1, max1] = [0, 5];
       const [min2, max2] = [6, 3];
 
-      const actual1 = myRange(min1, max1);
+      const actual1 = myRange(min1, max1); //should true
       const expected1 = [0, 1, 2, 3, 4, 5];
-      const actual2 = myRange(min2, max2);
+      const actual2 = myRange(min2, max2); // should true
       const expected2 = [];
 
       expect(actual1).to.eql(expected1);
@@ -79,16 +79,16 @@ describe("myRange(min, max, step)", () => {
 
   context("if step is provided", () => {
     it("should return the correct array", () => {
-      const [min1, max1, step1] = [0, 5, 1];
+      const [min1, max1, step1] = [0, 5, 1]; 
       const [min2, max2, step2] = [0, 5, 2];
       const [min3, max3, step3] = [9, 5, 2];
 
-      const actual1 = myRange(min1, max1, step1);
+      const actual1 = myRange(min1, max1, step1); //should true
       const expected1 = [0, 1, 2, 3, 4, 5];
-      const actual2 = myRange(min2, max2, step2);
-      const expected2 = [0, 2, 4];
-      const actual3 = myRange(min3, max3, step3);
-      const expected3 = [];
+      const actual2 = myRange(min2, max2, step2); //should true
+      const expected2 = [0, 2, 4]; 
+      const actual3 = myRange(min3, max3, step3); //should true
+      const expected3 = []; 
 
       expect(actual1).to.eql(expected1);
       expect(actual2).to.eql(expected2);
